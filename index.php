9d141c81d34e387261afb910a407779803d0f7d1
Duy_Thanh <thanh08052001@gmail.com>
2023-03-27 10:31:16 +0700

    upload file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Thông tin sinh viên</title>
+  </head>
+  <body>
+    <h1>Thông tin sinh viên</h1>
+    <?php
+      $hoTen = "Nguyễn Duy Thanh";
+      $maSV = "B1909984";
+      $email = "ThanhB1909984@student.ctu.edu.vn";
+      $dienThoai = "0921080500";
+    ?>
+    <p>Họ tên: <?php echo $hoTen; ?></p>
+    <p>Mã số sinh viên: <?php echo $maSV; ?></p>
+    <p>Địa chỉ email: <?php echo $email; ?></p>
+    <p>Điện thoại: <?php echo $dienThoai; ?></p>
+  </body>
+</html>
